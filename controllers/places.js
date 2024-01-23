069088ba6f64b72798fb72976b0148844e0b5ee1
user name <user email>
2024-01-23 18:29:22 -0500

    hello

--- controllers/places.js
+++ controllers/places.js
@@ -2,7 +2,9 @@ const router = require('express').Router()
 
 // More code here in a moment
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    let places = []
+    res.render('places/index')
+    //GET /places
 })
 
 module.exports = router
