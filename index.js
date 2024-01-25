aca1187dccf5dd241f8bc24b1b226256657fa410
user name <user email>
2024-01-24 22:25:12 -0500

    Adding styles to Rest-Rant

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))//to let Express know the name of the static folder
 
+//Controllers and Routers
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) =>{
@@ -16,5 +19,5 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
-
+//Listen for Connections
 app.listen(process.env.PORT)
