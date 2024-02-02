16eda917ca43fb9eaa3ff7affd9c4060620e1ba8
user name <user email>
2024-02-01 21:44:29 -0500

    Updating Bootstrap

--- views/default.jsx
+++ views/default.jsx
@@ -5,7 +5,8 @@ function Def (html) {
         <html>
             <head>
             <title>Title</title>
-           <link rel="stylesheet" href= "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"/>
+            
+            <link rel= "stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
                 <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
