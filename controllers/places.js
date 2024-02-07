40bd151e0c374e8eab6e97b9b48e90278df38bd6
user name <user email>
2024-02-07 05:11:45 -0500

    implementing mongoose ODM

--- controllers/places.js
+++ controllers/places.js
@@ -6,7 +6,7 @@ const db = require('../models')
 
 
 
-//Index
+//Index// A List
 router.get('/', (req, res) => {
     db.Place.find()
       .then((places) => {
@@ -19,9 +19,7 @@ router.get('/', (req, res) => {
       
  })
 
-
-
-//POST
+//POST//Create
 
 router.post('/', (req, res) => {
   db.Place.create(req.body)
@@ -33,8 +31,6 @@ router.post('/', (req, res) => {
       res.render('error404')
   })
 })
-
-
 
 //New
 router.get('/new', (req, res) => {
@@ -54,9 +50,6 @@ router.get('/:id', (req, res) => {
       res.render('error404')
   })
 })
-
-
-
 
 
 //PUT
@@ -98,39 +91,71 @@ router.get('/:id/edit', (req, res) => {
 })
 
 
+//router.post('/:id/rant', (req, res) => {
+  //res.send('GET /places/:id/rant stub')
+//})
+
+
+// POST RANT
 router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
+  const { id } = req.params
+  const { rant } = req.body
+  // Will find the place by ID and update its 'rant' field with the new rant
+  db.Place.findByIdAndUpdate(id, { $push: { rants: rant } }, { new: true })
+    .then(place => {
+      res.redirect(`/places/${id}`) // Will redirect back to the place's show page after posting the rant
+    })
+    .catch(err => {
+      console.log('Error posting rant:', err)
+      res.render('error404')
+    })
 })
 
 
 
 //DELETE
-router.delete('/:id/rant/:rant:Id', (req, res) => {
-     res.send('GET /places/:id/rant/:rantId stub')
+//router.delete('/:id/rant/:rant:Id', (req, res) => {
+//     res.send('GET /places/:id/rant/:rantId stub')
+//})
+
+// DELETE RANT
+router.delete('/:id/rant/:rantId', (req, res) => {
+  const { id, rantId } = req.params;
+  // Finds the place by ID and updates it's 'rants' array by removing the rant with the given ID
+  db.Place.findByIdAndUpdate(id, { $pull: { rants: { _id: rantId } } }, { new: true })
+    .then(place => {
+      res.redirect(`/places/${id}`); // will redirect back to the place's show page after deleting the rant
+    })
+    .catch(err => {
+      console.log('Error deleting rant:', err)
+      res.render('error404')
+    })
 })
 
 
 
-//.get('/', (req, res) => {
- // let places = [{
- //  name: 'H-Thai-ML',
- //   city: 'Seattle',
- //  state: 'WA',
- //   cuisines: 'Thai, Pan-Asian',
- //   pic: '/images/images/emy-XoByiBymX20-unsplash (1).jpg'
+
+
+  .get('/', (req, res) => {
+    let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/public/images/emy-XoByiBymX20-unsplash (1).jpg'
   
     
-  //},
- // {
- //   name: 'Coding Cat Cafe',
- //   city: 'Phoenix',
-  //  state: 'AZ',
-  //  cuisines: 'Coffee, Bakery',
-  //  pic: '/images/images/mink-mingle-qZ5lPCPvdXE-unsplash.jpg'  
-  //}]
+  },
+  {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/public/images/mink-mingle-qZ5lPCPvdXE-unsplash.jpg'
+  }]
    
- //   res.render('places/index',{places})
- // })                               
+    res.render('places/index',{places})
+  })                               
 
 
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,17 +5,18 @@ function show (data) {
     return (
         <Def>
 
-          <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+          <a href={`/places/${data.place.id}/edit`} className="btn btn-warning"> 
               Edit
           </a>     
  
-          <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+          <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
             <button type="submit" className="btn btn-danger">
                Delete
             </button>
           </form> 
           <main>
             <h1>{ data.place.name }</h1>
+            <img src={data.place.pic} alt={data.place.name} />
           </main>
         </Def>
     )
